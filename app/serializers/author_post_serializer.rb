fb4d135cbe043929b72573c3bc47586e10c36dcf
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-10-17 06:20:03 +0100

    implement nested route with include in controller

--- app/serializers/author_post_serializer.rb
+++ app/serializers/author_post_serializer.rb
@@ -1,8 +1,17 @@
 class AuthorPostSerializer < ActiveModel::Serializer
-  attributes :title, :short_content, :tags
+  attributes :title, :short_content
+  has_many :tags
 
   def short_content
     self.object.content.truncate(43)
   end
+
+  # def tags
+  #   self.object.tags.map do |tag|
+  #     {  
+  #       name: tag.name  
+  #     }
+  #   end 
+  # end
 
 end
